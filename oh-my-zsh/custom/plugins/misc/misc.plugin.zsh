7c8410321a1c7f9c56f72765aa491c5b7f8c0936
Dave Rose <dave.rose@shopify.com>
2019-01-17 15:02:52 -0500

    Update mov2gif helper method

--- oh-my-zsh/custom/plugins/misc/misc.plugin.zsh
+++ oh-my-zsh/custom/plugins/misc/misc.plugin.zsh
@@ -5,7 +5,7 @@ mov2gif() {
     else
         output=${2}
     fi
-    ffmpeg -i $1 -vf scale=1080:-1:flags=lanczos,palettegen -f palette.png
-    ffmpeg -i $1 -i -i palette.png -filter_complex "fps=15,scale=1080:-1:flags=lanczos[x];[x][1:v]paletteuse" $2
+    ffmpeg -i $1 -vf fps=15,scale=1080:-1:flags=lanczos,palettegen palette.png
+    ffmpeg -i $1 -i palette.png -filter_complex "fps=15,scale=1080:-1:flags=lanczos[x];[x] [1:v]paletteuse" ${output}
     rm palette.png
 }
