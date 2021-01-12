8b31f072b5bce806baba99b4164e1eacfe0ff78a
Dave Rose <dave.rose@shopify.com>
2021-01-11 19:37:02 -0500

    Fix Install scripts

--- fonts/install.sh
+++ fonts/install.sh
@@ -3,9 +3,9 @@
 if command -v brew @>/dev/null ; then
     #Install fonts using HomeBrew - MacOS
     brew tap homebrew/cask-fonts
-    brew cask install font-firacode-nerd-font-mono
-    brew cask install font-firacode-nerd-font
-    brew cask install font-fira-code
-    brew cask install font-xkcd-script
-    brew cask install font-xkcd
+    brew install --cask font-firacode-nerd-font-mono
+    brew install --cask font-firacode-nerd-font
+    brew install --cask font-fira-code
+    brew install --cask font-xkcd-script
+    brew install --cask font-xkcd
 fi
