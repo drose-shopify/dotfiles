cd0633217d7a8785868994abf5bb77bcfd42bb3e
Dave Rose <dave.rose@shopify.com>
2022-03-01 15:00:50 -0500

    Update spin functions to include some spin helpers

--- zsh/shopify_local_functions.zsh
+++ zsh/shopify_local_functions.zsh
@@ -1,15 +1,15 @@
 ss() {
-    local workspace=$(spin list -o text | fzf +m -1)
+    local workspace=$(spin list -o name | fzf +m -1)
     spin shell $workspace
 }
 
 so() {
-    local workspace=$(spin list -o text | fzf +m -1)
+    local workspace=$(spin list -o name | fzf +m -1)
     spin open $workspace
 }
 
 sb() {
-    local workspace=$(spin list -o text | fzf +m -1)
+    local workspace=$(spin list -o name | fzf +m -1)
     open "https://shop1.${workspace}.dave-rose.us.spin.dev"
 }
 
--- zsh/spin_functions.zsh
+++ zsh/spin_functions.zsh
@@ -1,3 +1,22 @@
 alias g='git'
 alias e='nvim'
 alias bat='batcat'
+
+
+restart() {
+    ls ~/src/github.com/Shopify | fzf --multi | while read line; do
+      systemctl list-units --all | rg proc-shopify--$line | awk '{print $1}' | xargs  systemctl restart
+    done
+}
+
+status() {
+    ls ~/src/github.com/Shopify | fzf --multi | while read line; do
+      systemctl list-units --all | rg proc-shopify--$line | awk '{print $1}' | xargs  systemctl restart
+    done
+}
+
+stop() {
+    ls ~/src/github.com/Shopify | fzf --multi | while read line; do
+      systemctl list-units --all | rg proc-shopify--$line | awk '{print $1}' | xargs  systemctl restart
+    done
+}
